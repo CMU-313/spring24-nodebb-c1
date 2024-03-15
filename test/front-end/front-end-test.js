8269bd71ab0c88e6f22a0a001102544f1cfd23fb
Dillon Shu <dillons@andrew.cmu.edu>
2024-03-15 16:47:30 -0400

    added additional screenshot capture for puppeteer

--- test/front-end/front-end-test.js
+++ test/front-end/front-end-test.js
@@ -4,19 +4,20 @@ const puppeteer = require('puppeteer');
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
   await page.goto('http://localhost:4568/login');
+  await page.screenshot({ path: 'login.png' });
   await page.type('#username', 'dillons');
   await page.type('#password', 'dillons');
+  await page.screenshot({ path: 'login-info.png' });
   await page.click('#login');
   await page.waitForNavigation();
 
-  await page.screenshot({ path: 'login.png' });
+  await page.screenshot({ path: 'post-login.png' });
 
   await page.goto('http://localhost:4568/category/2/general-discussion');
   await page.click('#new_topic');
   await page.waitForNavigation();
 
   await page.screenshot({ path: 'new-post.png' });
-
 
   await browser.close();
 })();
