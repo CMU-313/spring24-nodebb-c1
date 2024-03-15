ddf4f96c883bab8f14af6ce7f0067060b1e5f25a
Dillon Shu <dillons@andrew.cmu.edu>
2024-03-15 16:23:50 -0400

    installed puppeteer and began testing

---
+++ test/front-end/front-end-test.js
@@ -0,0 +1,22 @@
+const puppeteer = require('puppeteer');
+
+(async () => {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto('http://localhost:4568/login');
+  await page.type('#username', 'dillons');
+  await page.type('#password', 'dillons');
+  await page.click('#login');
+  await page.waitForNavigation();
+
+  await page.screenshot({ path: 'login.png' });
+
+  await page.goto('http://localhost:4568/category/2/general-discussion');
+  await page.click('#new_topic');
+  await page.waitForNavigation();
+
+  await page.screenshot({ path: 'new-post.png' });
+
+
+  await browser.close();
+})();
