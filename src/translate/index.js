575509f889d1779d92ea2f70cfea76bec40c9b43
Dillon Shu <dillons@andrew.cmu.edu>
2024-04-01 16:19:30 -0400

    temporarily replaced environment variable with link

--- src/translate/index.js
+++ src/translate/index.js
@@ -4,7 +4,8 @@ var request = require('request')
 const translatorApi = module.exports;
 
 translatorApi.translate = async function (postData) {
-    const response = await fetch(process.env.TRANSLATOR_API+'/?content='+postData.content);
+    //const response = await fetch(process.env.TRANSLATOR_API+'/?content='+postData.content);
+    const response = await fetch('https://translator-service-cwmfb7fbpq-uc.a.run.app//?content='+postData.content);
     const data = await response.json();
     return [data["is_english"], data["translated_content"]]
 }
