13e376e4aa5e0813c7afb4dfaaa0dae0b381c409
Joyce Hong <joyce.hong8@gmail.com>
2024-04-01 20:20:45 -0400

    fix lint

--- src/translate/index.js
+++ src/translate/index.js
@@ -1,11 +1,12 @@
-const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
-var request = require('request')
+'use strict';
+
+const fetch = require('node-fetch');
 
 const translatorApi = module.exports;
 
 translatorApi.translate = async function (postData) {
-    //const response = await fetch(process.env.TRANSLATOR_API+'/?content='+postData.content);
-    const response = await fetch('https://translator-service-cwmfb7fbpq-uc.a.run.app//?content='+postData.content);
+    // const response = await fetch(process.env.TRANSLATOR_API+'/?content='+postData.content);
+    const response = await fetch(`https://translator-service-cwmfb7fbpq-uc.a.run.app//?content=${postData.content}`);
     const data = await response.json();
-    return [data["is_english"], data["translated_content"]]
-}
+    return [data.is_english, data.translated_content];
+};
